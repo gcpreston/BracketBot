b804d0fb32955b7187bbd683085084825f633c2c
Graham Preston <preston.gr@husky.neu.edu>
2019-07-27 14:10:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BracketBot
+Discord bot for providing the latest Challonge bracket from a specified page. Requires username and API token from page to fetch bracket for. Type "!bracket" for the latest bracket.
---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require("axios");
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+client.on("message", async message => {
+    if (message.author.bot) return;
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === "bracket") {
+        console.log(message.author.username + " asked for the bracket");
+        axios({
+            method: "get",
+            url: "https://api.challonge.com/v1/tournaments.json",
+            auth: {
+                username: config.challongeUsername,
+                password: config.challongeToken
+            }
+        })
+            .then(response => {
+                message.reply("Latest tournament: " + response.data.pop().tournament["full_challonge_url"]);
+            })
+            .catch(error => {
+                console.log("ERROR HAS OCCURRED\n");
+                console.log(error);
+            });
+    }
+});
+
+client.login(config.token);
+console.log("BracketBot started");
